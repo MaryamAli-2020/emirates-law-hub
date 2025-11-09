fa7a3487a010a29348be30b513e56be31f7f4769
Maryam Mohammed <mhralikeamoon@gmail.com>
2025-11-09 17:37:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9753301176-ca182",
+  "appId": "1:589854796843:web:05ae380e5e82366d774e6e",
+  "apiKey": "AIzaSyCNwQNAgEiAFFIJRCzZfBnK3fxTuFcl-28",
+  "authDomain": "studio-9753301176-ca182.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "589854796843"
+};
